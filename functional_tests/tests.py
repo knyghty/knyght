d92ae4699991d8a9ff0d6fcb18085f255e5faf0b
Tom Carrick <knyght@knyg.ht>
2015-05-12 15:36:13 +0100

    Add header

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -11,6 +11,13 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_homepage_title(self):
+    def test_homepage_browse(self):
+        # Bertram lands on the hompage of the site.
         self.browser.get(self.live_server_url)
+
+        # He sees knyg.ht in the title.
         self.assertIn('knyg.ht', self.browser.title)
+
+        # He also sees knyg.ht in the content.
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('knyg.ht', body.text)
